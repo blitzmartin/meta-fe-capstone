17e67a8aa0bdc3bdf1d763d86a3fea64546ea818
annalisa4sec <122273976+annalisa4sec@users.noreply.github.com>
2025-01-03 20:12:42 +0100

    add useState and useEffect to fetch image from Unsplash (issues)

--- src/pages/menu/MenuPage.jsx
+++ src/pages/menu/MenuPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { fetchImages } from '../../api';
 import { MainContainer, Section, Separator } from '../../shared';
 import { menuItems } from '../../utils/constants';
@@ -14,9 +15,19 @@ export const MenuPage = () => {
 
 
 const MenuItem = ({ item }) => {
+  const [image, setImage] = useState(null); // State to store the fetched image
 
-     const image = fetchImages(1);
-     console.log(image)
+  useEffect(() => {
+    const fetchImageData = async () => {
+      const images = await fetchImages(1);
+      if (images.length > 0) {
+        setImage(images[0]);
+      }
+    };
+    console.log(image)
+    fetchImageData();
+  }, [image]);
+
      return (
           <div className='flex flex-col gap-6 text-gray-800 max-w-2xl mx-auto'>
                <Separator />
@@ -26,7 +37,7 @@ const MenuItem = ({ item }) => {
                          <p className='text-sm'>{item.description}</p>
                          <p className='font-bold text-gray-600'>${item.price.toString()}</p>
                     </div>
-                    <img alt={image[0].alt} src={image[0].src} />
+                 {image && <img alt={image.alt} src={image.src} />}
                </div>
                <div></div>
           </div>
